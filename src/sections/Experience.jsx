20c157794833b83ed9f34b2e605da83d1a63136c
Nicolas Marin <nmarinmunoz.pro@gmail.com>
2024-10-29 13:14:27 +0100

    main: update developper model

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -16,8 +16,7 @@ const Experience = () => {
                     <div className="work-canvas">
                         <Canvas>
                             <ambientLight intensity={7} />
-                            <spotLight intensity={[10, 10, 10]} angle={0.15} penumbra={1} />
-                            <directionalLight position={[10, 10, 10]} intensity={1} />
+                            <directionalLight position={[10, 10, 10]} intensity={4} />
                             <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} />
                             <Suspense fallback={<CanvasLoader />}>
                                 <Developer animationName={animationName} position-y={-3} scale={3} />
